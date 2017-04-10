0aa391c4668022b213b3e901be4692b32639fb81
TrAnd <magtuxgit@gmail.com>
2017-04-10 16:04:30 +0300

    working tweet reader

---
+++ Smashtag/TweetTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by Andrij Trubchanin on 4/10/17.
+//  Copyright © 2017 Andrij Trubchanin. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewController: UITableViewController {
+    private var tweets = [Array<Twitter.Tweet>]() { // you can use Tweet as long as you don't have your own Tweet class
+        didSet {
+            print(tweets)
+        }
+    }
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            tableView.reloadData()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private func twitterRequest() -> Twitter.Request? {
+        if let query = searchText, !query.isEmpty {
+            return Twitter.Request(search: query, count: 100)
+        }
+        return nil
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func searchForTweets() {
+        if let request = twitterRequest() {
+            lastTwitterRequest = request
+            request.fetchTweets { [weak self] newTweets in
+                DispatchQueue.main.async {
+                    // dispatch it back because tableView is an UI stuff
+                    if request == self?.lastTwitterRequest {
+                        self?.tweets.insert(newTweets, at: 0)
+                        self?.tableView.insertSections([0], with: .fade)
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return tweets.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets[section].count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweet", for: indexPath)
+
+        let tweet = tweets[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = tweet.text
+        cell.detailTextLabel?.text = tweet.user.name
+        
+        return cell
+    }
+}
